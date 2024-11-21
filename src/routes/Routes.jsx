1d083d56e2f56e46e381ca4e6d09dc0c6faf48a3
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2024-11-21 17:44:32 +0000

    create routing to different pages

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,4 +1,17 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from "../components/Home";
-import Contact from "../components/Contact";
 
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "../pages/Home";
+import Contact from "../pages/Contact";
+
+const AppRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/contact" element={<Contact/>} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRoutes;
