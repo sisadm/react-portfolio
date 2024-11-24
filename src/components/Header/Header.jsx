29f22c8ce1fb8287f1cac1372955343a288c83f0
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2024-11-24 19:57:27 +0000

    tailwind

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,35 @@
+import '../../index.css'
 import './Header.css'
 import {whiteBtn, blackBtn}  from "../Button/Button.jsx";
 
 
 const Header = () => {
     return (
-        <header className="Header">
+        <>
+            <header className="Header">
+                <div>
+                    {whiteBtn("Copy")}
+                </div>
+                <nav className="nav-bar">
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="/">Contact</a></li>
+                    </ul>
+                </nav>
+            </header>
+
             <div>
-                {whiteBtn("Copy")}
+                <div>
+                    <img src="" alt=""/>
+                </div>
+                <div>
+                    <h1 className="size-16">Front-End Developer</h1>
+                    {blackBtn("Projects", "project-btn")}
+                </div>
             </div>
-            <nav className="nav-bar">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="/">Contact</a></li>
-                </ul>
-            </nav>
-        </header>
+        </>
+
     )
 }
 
